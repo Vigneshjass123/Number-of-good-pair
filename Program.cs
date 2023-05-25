6d03def1048c175abaf3a07a105965ed67d80d7b
Cyntexia <Cyntexia@DESKTOP-R5AAUJH>
2023-05-26 01:14:10 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_of_Good_Pairs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 1, 1, 3 };
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = i + 1; j < a.Length; j++)
+                {
+                    if (a[i] == a[j])
+
+                    {
+                        Console.Write(a[i]);
+                    }
+                    
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
+    
+
+    
+
